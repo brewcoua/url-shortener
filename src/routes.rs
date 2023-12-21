1576794d0d7a1de87814f3d40deb916768c913d0
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2023-12-21 15:53:29 +0100

    feat(routes): add health check route (/health/check)

--- src/routes.rs
+++ src/routes.rs
@@ -2,12 +2,14 @@ use axum::Router;
 use axum::routing::get;
 use crate::state::AppState;
 
-pub mod redirect;
-pub mod root;
+mod redirect;
+mod root;
+mod health;
 
 pub fn declare(state: AppState) -> Router {
     Router::new()
         .route("/", get(root::root))
+        .route("/health/check", get(health::health))
         .route("/:slug", get(redirect::redirect))
         .with_state(state)
 }
